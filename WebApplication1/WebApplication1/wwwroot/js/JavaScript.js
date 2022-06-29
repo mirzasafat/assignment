96b22821ef5e8798f722f56319a9927add0bea84
mirzasafat <108406883+mirzasafat@users.noreply.github.com>
2022-06-29 07:43:16 -0700

    Add files via upload

---
+++ WebApplication1/WebApplication1/wwwroot/js/JavaScript.js
@@ -0,0 +1,15 @@
+﻿var currind = 0;
+slideNow();
+
+function slideNow() {
+    var i;
+    var x = document.getElementsByClassName("Slides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    currind++;
+    if (currind > x.length) { currind = 1 }
+    x[currind - 1].style.display = "block";
+    setTimeout(slideNow, 3000);
+}
+
